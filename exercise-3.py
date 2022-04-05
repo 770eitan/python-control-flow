60eec5713e2ce54e2dc12f24973df09ad6f7f1bb
Eitan Shmuel Corenblum <97841601+770eitan@users.noreply.github.com>
2022-04-05 17:22:30 -0400

    Add files via upload

---
+++ exercise-3.py
@@ -0,0 +1,19 @@
+# exercise-03 Calculate Dog Years
+
+# Write the code that:
+# 1. Prompts the user to enter a dog's age in human years like this:
+#      Input a dog's age in human years: 
+# 2. Calculates the equivalent dog years, where:
+#      - The first two years count as 10 years each
+#      - Any remaining years count as 7 years each
+# 3. Prints the answer in the following format:
+#      The dog's age in dog years is xx
+
+# Hint:  Use the int() function to convert the string returned from input() into an integer
+dogs_age=input("Input a dog's age in human years")
+dogs_number= int(dogs_age)
+if dogs_number>2:
+    dogs_years = (dogs_number-2)*7+20
+else: 
+    dogs_years=dogs_number*10
+print(f"The dog's age in dog years is {dogs_years}")
